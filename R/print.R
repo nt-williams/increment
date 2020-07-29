03c71a8cea2693dd50cd2946f3a5dd1204bb7292
Nick Williams <ntwilliams.personal@gmail.com>
2020-07-29 16:54:13 -0400

    adding pvalue placeholder in print

--- R/print.R
+++ R/print.R
@@ -5,6 +5,7 @@ print.increment <- function(x, ...) {
   cat("\n")
   cat("           Confidence level: 95%\n")
   cat("     Multiplier Bootstrap C:", round(x$mult_cv, 2), "\n")
+  cat(" Test of no effect, p-value:\n")
   cat("First 6 increment estimates:\n")
   cat("\n")
   print(head(round(x$estimates, 2)))
